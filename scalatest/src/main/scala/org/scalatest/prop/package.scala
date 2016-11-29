1836987cdf52e55f08abffd670e6396e57cdc187
bvenners <bill@artima.com>
2016-11-29 11:01:42 -0800

    Added a generator for tuple2 with a few tests and one for tuple3 with as yet no tests. Started adding a package object for prop. Created a Generator2 class that will support a nice way to make generators that compose shrinks.

--- scalatest/src/main/scala/org/scalatest/prop/package.scala
+++ scalatest/src/main/scala/org/scalatest/prop/package.scala
@@ -15,12 +15,52 @@
  */
 package org.scalatest
 
-import org.scalactic.anyvals._
-import scala.annotation.tailrec
-import scala.reflect.runtime.universe.TypeTag
-
 package object prop {
+  // If I give them a method that offers (A, B) => C. C => A, and C => B functions, then
+  // they can get composed shrinkers. And that's the general one:
+  //
+  // trait Generator2[A, B, C](abc: (A, B) => C, ca: C => A, cb: C => B)(genOfA: Generator[A], genOfB: Generator[B]) extends Generator[C] {
+  // }
+  def gen[A, B, C](f: (A, B) => C)(implicit genOfA: Generator[A], genOfB: Generator[B]): Generator[C] = {
+    val tupGen: Generator[(A, B)] = Generator.tuple2Generator[A, B]
+    for (tup <- tupGen) yield f(tup._1, tup._2)
+  }
 
+  // gen(
+  //   (name: String, age: Int) => Person(name, age), 
+  //   (p: Person) => p.name,
+  //   (p: Person) => p.age
+  // )  // This one takes the implicits at the back. I'd do overloading again as is my wont.
+
+  // gen { (name: String, age: Int) => Person(name, age), }  // This one also takes the implicits at the back. I'd do overloading again as is my wont. It uses canonicals in the shrink/grow method.
+ // I wonder if I could just leave this off. They can get this with a for expression.
+
+  // gen(nonEmptyStringValues, posZIntValues)(
+  //   (name: String, age: Int) => Person(name, age), 
+  //   (p: Person) => p.name,
+  //   (p: Person) => p.age
+  // )
+  // Here I couldn't overload. Hmm.
+
+  /*
+    ints
+    chars
+    longs
+    bytes
+    shorts
+    floats
+    doubles
+    booleans
+    posZInts
+    posZIntValues
+    nonEmptyStrings
+    nonEmptyStringValues
+    and so on...
+
+    mabye shrink can be growTo(target: T
+    grower(value: T, ...)
+    grow(value: T, ...)
+  */
 }
 
 

e3411232f8aecfd8667a9051e3029fd00ac9b9b4
cheeseng <cheeseng@amaseng.com>
2016-12-20 23:02:47 +0800

    Generate NonZeroLong and NonZeroLongMacro.

--- project/GenAnyVals.scala
+++ project/GenAnyVals.scala
@@ -20,7 +20,7 @@ import java.io.BufferedWriter
 
 object GenAnyVals {
 
-  def genMacro(targetDir: File, typeName: String, typeBooleanExpr: String): File = {
+  def genMacro(targetDir: File, primitiveTypeName: String, typeName: String, typeBooleanExpr: String): File = {
     val content =
       s"""/*
         | * Copyright 2001-2016 Artima, Inc.
@@ -44,15 +44,15 @@ object GenAnyVals {
         |
         |private[scalactic] object ${typeName}Macro extends CompileTimeAssertions {
         |
-        |  def isValid(i: Int): Boolean = $typeBooleanExpr
+        |  def isValid(i: $primitiveTypeName): Boolean = $typeBooleanExpr
         |
-        |  def apply(c: Context)(value: c.Expr[Int]): c.Expr[$typeName] = {
+        |  def apply(c: Context)(value: c.Expr[$primitiveTypeName]): c.Expr[$typeName] = {
         |    val notValidMsg = Resources.notValid$typeName
         |    val notLiteralMsg = Resources.notLiteral$typeName
         |
         |    import c.universe._
         |
-        |    ensureValidIntLiteral(c)(value, notValidMsg, notLiteralMsg)(isValid)
+        |    ensureValid${primitiveTypeName}Literal(c)(value, notValidMsg, notLiteralMsg)(isValid)
         |    reify { $typeName.ensuringValid(value.splice) }
         |  }
         |}
@@ -69,7 +69,8 @@ object GenAnyVals {
   }
 
   def genIntAnyVal(targetDir: File, typeName: String, typeDesc: String, typeNote: String, typeBooleanExpr: String, typeValidExample: String, typeInvalidExample: String,
-                   typeMinValue: String, typeMinValueNumber: String, typeMaxValue: String, typeMaxValueNumber: String, widensToTypes: Seq[String]): Seq[File] = {
+                   typeValidValue: String, typeInvalidValue: String, typeMinValue: String, typeMinValueNumber: String, typeMaxValue: String, typeMaxValueNumber: String,
+                   widensToTypes: Seq[String]): List[File] = {
     val templateSource = scala.io.Source.fromFile("project/templates/IntAnyVal.template")
     val templateText = try templateSource.mkString finally templateSource.close()
     val st = new org.antlr.stringtemplate.StringTemplate(templateText)
@@ -80,6 +81,8 @@ object GenAnyVals {
     st.setAttribute("typeBooleanExpr", typeBooleanExpr)
     st.setAttribute("typeValidExample", typeValidExample)
     st.setAttribute("typeInvalidExample", typeInvalidExample)
+    st.setAttribute("typeValidValue", typeValidValue)
+    st.setAttribute("typeInvalidValue", typeInvalidValue)
     st.setAttribute("typeMinValue", typeMinValue)
     st.setAttribute("typeMinValueNumber", typeMinValueNumber)
     st.setAttribute("typeMaxValue", typeMaxValue)
@@ -107,13 +110,61 @@ object GenAnyVals {
     bw.flush()
     bw.close()
     println("Generated: " + targetFile.getAbsolutePath)
-    List(targetFile, genMacro(targetDir, typeName, typeBooleanExpr))
+    List(targetFile, genMacro(targetDir, "Int", typeName, typeBooleanExpr))
+  }
+
+  def genLongAnyVal(targetDir: File, typeName: String, typeDesc: String, typeNote: String, typeBooleanExpr: String, typeValidExample: String, typeInvalidExample: String,
+                   typeValidValue: String, typeInvalidValue: String, typeMinValue: String, typeMinValueNumber: String, typeMaxValue: String, typeMaxValueNumber: String,
+                   widensToTypes: Seq[String]): List[File] = {
+    val templateSource = scala.io.Source.fromFile("project/templates/LongAnyVal.template")
+    val templateText = try templateSource.mkString finally templateSource.close()
+    val st = new org.antlr.stringtemplate.StringTemplate(templateText)
+
+    st.setAttribute("typeName", typeName)
+    st.setAttribute("typeDesc", typeDesc)
+    st.setAttribute("typeNote", typeNote)
+    st.setAttribute("typeBooleanExpr", typeBooleanExpr)
+    st.setAttribute("typeValidExample", typeValidExample)
+    st.setAttribute("typeInvalidExample", typeInvalidExample)
+    st.setAttribute("typeValidValue", typeValidValue)
+    st.setAttribute("typeInvalidValue", typeInvalidValue)
+    st.setAttribute("typeMinValue", typeMinValue)
+    st.setAttribute("typeMinValueNumber", typeMinValueNumber)
+    st.setAttribute("typeMaxValue", typeMaxValue)
+    st.setAttribute("typeMaxValueNumber", typeMaxValueNumber)
+
+    val widensToOtherAnyVals =
+      widensToTypes.map { targetType =>
+        s"""/**
+            |   * Implicit widening conversion from <code>$typeName</code> to <code>$targetType</code>.
+            |   *
+            |   * @param pos the <code>$typeName</code> to widen
+            |   * @return the <code>$targetType</code> widen from <code>$typeName</code>.
+            |   */
+            |  implicit def widenTo$targetType(pos: $typeName): $targetType = $targetType.ensuringValid(pos.value)
+            |
+        """.stripMargin
+      }.mkString
+
+    st.setAttribute("widensToOtherAnyVals", widensToOtherAnyVals)
+
+    val targetFile = new File(targetDir, typeName + ".scala")
+    val bw = new BufferedWriter(new FileWriter(targetFile))
+
+    bw.write(st.toString)
+    bw.flush()
+    bw.close()
+    println("Generated: " + targetFile.getAbsolutePath)
+    List(targetFile, genMacro(targetDir, "Long", typeName, typeBooleanExpr))
   }
 
   def genMain(dir: File, version: String, scalaVersion: String): Seq[File] = {
     dir.mkdirs()
-    genIntAnyVal(dir, "NonZeroInt", "non-zero", "Note: a <code>NonZeroInt</code> may not equal 0.", "i != 0", "NonZeroInt(42)", "NonZeroInt(0)", "Int.MinValue", "-2147483648",
-                 "Int.MaxValue", "2147483647", List("NonZeroLong"))
+
+    genIntAnyVal(dir, "NonZeroInt", "non-zero", "Note: a <code>NonZeroInt</code> may not equal 0.", "i != 0", "NonZeroInt(42)", "NonZeroInt(0)", "42", "0", "Int.MinValue", "-2147483648",
+                 "Int.MaxValue", "2147483647", List("NonZeroLong")) :::
+    genLongAnyVal(dir, "NonZeroLong", "non-zero", "Note: a <code>NonZeroLong</code> may not equal 0.", "i != 0L", "NonZeroLong(42)", "NonZeroLong(0)", "42", "0", "Long.MinValue", "-9223372036854775808",
+                  "Long.MaxValue", "9223372036854775807", List())
   }
 
 }
